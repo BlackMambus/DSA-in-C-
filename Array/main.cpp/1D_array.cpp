ae15724fdfb45ca5b7362366e78279643c0b8390
DEEPANSHU <blackmambus04@gmail.com>
2025-03-28 08:45:29 +0530

    Add files via upload

---
+++ Array/main.cpp/1D_array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //1D Array
+int array[5]={1,2,3,4,5};
+
+cout<<array[4];
+
+return 0;
+}
+
---
+++ Array/main.cpp/2D_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //2D array
+    int arr[2][2]={
+        {20,30}  //00 01
+    };
+    cout<<arr[0][0];
+
+    return 0;
+}
+
+
